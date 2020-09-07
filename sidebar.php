f55297fb333cb6773afdc58945f841a8d91f062c
RubelHasan <rubelhasan306@gmail.com>
2020-09-08 03:37:22 +0600

    Creted sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,38 @@
+<div class="site-sidebar">
+                        <div class="single-sidebar">
+                            <form action="#">
+                                <input type="text" placeholder="Search" required>
+                                <button type="submit"><i class="fa fa-search"></i></button>
+                            </form>
+                        </div>
+                        <div class="single-sidebar">
+                            <h3>Categories</h3>
+                            <ul>
+                                <li><a href="#">Investment Planning</a></li>
+                                <li><a href="#">Development Planning</a></li>
+                                <li><a href="#">Business Strategy</a></li>
+                                <li><a href="#">Development Planning</a></li>
+                                <li><a href="#">Financial Planning</a></li>
+                            </ul>
+                        </div>
+                        <div class="single-sidebar">
+                            <h3>Follow us</h3>
+                            <div class="social-follow">
+                                <a href="#" class="facebook-bg"><i class="fa fa-facebook"></i>158K</a>
+                                <a href="#" class="twitter-bg"><i class="fa fa-twitter"></i>588K</a>
+                                <a href="#" class="instagram-bg"><i class="fa fa-instagram"></i>545K</a>
+                            </div>
+                        </div>
+                        <div class="single-sidebar">
+                            <h3>Archives</h3>
+                            <div class="archives">
+                                <a href="#">January<span>(09)</span></a>
+                                <a href="#">February<span>(21)</span></a>
+                                <a href="#">March<span>(43)</span></a>
+                                <a href="#">April<span>(32)</span></a>
+                            </div>
+                        </div>
+                        <div class="single-sidebar">
+                            <a href="#"><img src="assets/images/ad-banner.jpg" alt=""></a>
+                        </div>
+                    </div>
